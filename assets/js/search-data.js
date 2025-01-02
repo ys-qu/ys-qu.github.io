550a15e108de760bb6f475c6464eec205c9e9038
ys-qu <107629689+ys-qu@users.noreply.github.com>
2025-01-02 02:26:53 +0000

    Deploying to gh-pages from @ ys-qu/ys-qu.github.io@7be2c08b2f682551ed9bd8021ef6c7896fb56ab1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
