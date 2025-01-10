731469eb220fb10b43f67396321208fe05968a93
ys-qu <107629689+ys-qu@users.noreply.github.com>
2025-01-10 20:21:15 +0000

    Deploying to gh-pages from @ ys-qu/ys-qu.github.io@a90552d349b87be0b1406f096cce868ddb838d5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,16 +52,16 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{id: "news-jan-5-jan-9-2025-i-attended-the-2025-trb-annual-meeting",
-          title: 'Jan 5 ~ Jan 9, 2025, I attended the 2025 TRB Annual Meeting!...',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-attended-the-2025-trb-annual-meeting-tada-star",
+          title: 'I attended the 2025 TRB Annual Meeting!  :tada: :star:',
           description: "",
           section: "News",},{id: "projects-play-with-simulators",
           title: 'Play with Simulators',
