f59eb1afad63b5e4d5a10a7bd3e3a448e251a753
ys-qu <107629689+ys-qu@users.noreply.github.com>
2025-05-21 11:43:09 +0000

    Deploying to gh-pages from @ ys-qu/ys-qu.github.io@f7c32e6162a819f398ccdd98ef32540dd980f165 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Teaching experience and mentership.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
