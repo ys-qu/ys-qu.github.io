62871712f268b8200cadb44837d3a4b58cdf120a
ys-qu <107629689+ys-qu@users.noreply.github.com>
2026-06-02 04:48:35 +0000

    Deploying to gh-pages from @ ys-qu/ys-qu.github.io@0ba208427086aeb119479f521644a7db0be36ab0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -142,16 +142,16 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/Found-RL-accepted-by-Communications-in-Transportation-Research!/";
-            },},{id: "news-i-will-join-honda-ri-as-a-research-intern-this-summer",
-          title: '👏 I will join Honda-RI as a research intern this summer!',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/I-will-join-HONDA-RI-as-a-research-intern-this-summer/";
             },},{id: "news-we-successfully-organized-ngts-2026-at-purdue-university",
           title: '🎉 We successfully organized NGTS 2026 at Purdue University!',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/We-successfully-organized-NGTS-2026-at-Purdue-University!/";
+            },},{id: "news-i-joined-honda-ri-as-a-research-intern-this-summer",
+          title: '👏 I joined Honda-RI as a research intern this summer!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/I-joined-HONDA-RI-as-a-research-intern-this-summer/";
             },},{id: "projects-play-with-simulators",
           title: 'Play with Simulators',
           description: "I have been passionate about traffic simulation for a long time, exploring various simulators ranging from the classical VISSIM during my undergraduate studies to CARLA now as a Ph.D. student. This project aims to document and share insights in leveraging these tools for cutting-edge research.",
