3421a25d461ef6b8753627bc4fb090c4d1d6a4a3
ys-qu <107629689+ys-qu@users.noreply.github.com>
2026-06-02 04:17:58 +0000

    Deploying to gh-pages from @ ys-qu/ys-qu.github.io@94c3d9cecfd2162b113bdb8a78d041d2009bbdd1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -127,6 +127,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/I-will-join-HONDA-RI-as-a-research-intern-this-summer/";
+            },},{id: "news-we-successfully-organized-ngts-2026-at-purdue-university",
+          title: '🎉 We successfully organized NGTS 2026 at Purdue University!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/We-successfully-organized-NGTS-2026-at-Purdue-University!/";
             },},{id: "projects-play-with-simulators",
           title: 'Play with Simulators',
           description: "I have been passionate about traffic simulation for a long time, exploring various simulators ranging from the classical VISSIM during my undergraduate studies to CARLA now as a Ph.D. student. This project aims to document and share insights in leveraging these tools for cutting-edge research.",
